f802996098ed3d2bd120dec518bc16b40493179a
Rafael Malanco <rafael.malanco@softtek.com>
2022-12-13 17:02:04 -0600

    xd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { WelcomeComponent } from './pages/welcome/welcome.component';
-import { UnicornsComponent } from './entities/unicorns/unicorns.component';
+// import { LoginComponent } from './pages/login/login.component';
+// import { WelcomeComponent } from './pages/welcome/welcome.component';
+// import { UnicornsComponent } from './entities/unicorns/unicorns.component';
 
 const routes: Routes = [
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { LoginComponent } from './pages/login/login.component';
 import { UnicornsComponent } from './entities/unicorns/unicorns.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
